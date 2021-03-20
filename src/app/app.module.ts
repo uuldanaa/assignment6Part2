e4f74cd14aa0f63cde62fee0157b38f1de838674
LESOLEIL\USER <uuldanaa23@gmail.com>
2021-03-20 23:55:21 +0600

    the last commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddComponent} from './add/add.component';
+import { ResultsComponent } from './results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
